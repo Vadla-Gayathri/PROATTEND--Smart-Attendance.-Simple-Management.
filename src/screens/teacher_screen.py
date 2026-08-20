705bd2f56e9828642e6ad55c342c8e85a69b5d06
VadlaGaythri00 <vadlagayathri776@gmail.com>
2026-08-20 16:27:38 +0530

    commit

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -4,9 +4,8 @@ from src.components.header import header_dashboard
 
 def teacher_screen():
     style_background_dashboard()
-    style_base_layout() 
+    style_base_layout()
 
-    header_dashboard()
     c1, c2 = st.columns(2, vertical_alignment='center', gap= 'xxlarge')
     with c1:
         header_dashboard()
--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -60,7 +60,7 @@ def style_base_layout():
             h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 3.5rem !important;
-                line-height:1.1 !important;
+                line-height:1.1 1important;
                 margin-bottom:0rem !important;
             }
                 
@@ -70,8 +70,7 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                color: #5865F2 !important;
-                }
+            }
                 
             h3, h4, p {
                 font-family: 'Outfit', sans-serif;    
